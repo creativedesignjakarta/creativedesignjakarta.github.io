eeb0420d744227bf38a249dfcea1e0763a52149f
ID Devlop <168630298+iddevlop@users.noreply.github.com>
2024-10-11 16:39:48 +0700

    Create fBt181020Sc.js

---
+++ assets/js/fBt181020Sc.js
@@ -0,0 +1,2 @@
+/* License by Suryo DwiJayanto */
+var seTheme=localStorage.getItem("theme")?JSON.parse(localStorage.getItem("theme")):"";"true"==seTheme?document.body.style.background="#222":document.body.style.background="#F5F5F5";
